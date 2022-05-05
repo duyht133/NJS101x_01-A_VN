f2accdc4fd34987308b2451182324f5c6c5f3646
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-05 13:24:23 +0700

    lab1-7

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
 const http = require('http');
-const fs = require('fs');
-const rqListener = (req,res) =>{
-    const url = req.url;
-    const method = req.method;
-    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title> enter message </title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data',(data)=>{
-            console.log(data);
-            body.push(data);
-        })
-        req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync("message.txt",message)
-        })
-        res.statusCode = 302;
-        res.setHeader("location","/")
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title> My firt page </title></head>');
-    res.write('<body><h1> hello from my node.js server </h1></body>');
-    res.write('</html>');
-    res.end();
-}
-const server = http.createServer(rqListener);
+const routes = require('./routes');
+
+console.log(routes.sometext)
+
+const server = http.createServer(routes.handler);
 server.listen(3000);
---
+++ routes.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const requestHandler =(req,res)=>{
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title> enter message </title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/message' && method === 'POST'){
+        const body = [];
+        req.on('data',(data)=>{
+            console.log(data);
+            body.push(data);
+        })
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync("message.txt",message,(err)=>{
+                res.statusCode = 302;
+                res.setHeader("location","/")
+                return res.end();
+            });
+        });
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> My firt page </title></head>');
+    res.write('<body><h1> hello from my node.js server </h1></body>');
+    res.write('</html>');
+    res.end();
+};
+
+/* module.exports =requestHandler */
+
+module.exports = { // ưu tiên sử dụng cách này
+   handler: requestHandler,
+   sometext:'viet code chay'
+};
+
+/* exports.handler = requestHandler;
+exports.sometext = 'viet code chay' */
