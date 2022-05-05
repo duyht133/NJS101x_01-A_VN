75285aa8f0f9afc852b3c3af5d08b4aab460b5b4
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-05 11:35:18 +0700

    lab1-3

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const rqListener = (req,res) =>{
-    console.log(req)
+    console.log(req.url, req.method, req.headers);
+    /* process.exit(); */ // câu lệnh thoát.
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> My firt page </title></head>');
+    res.write('<body><h1> hello from my node.js server </h1></body>');
+    res.write('</html>');
+    res.end();
 }
 
 const server = http.createServer(rqListener);
