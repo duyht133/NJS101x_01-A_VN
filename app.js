434737c0a177b68aaac657aea5cdcf385fa0926f
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-07 02:23:22 +0700

    lab2-5

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const app = express(); // tạo biến app cho middleware (cú pháp bắc buộc)
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // sử dụng app với middleware use()
 app.use("/", (req, res, next) => {
-  console.log("chay o day");
   next();
 });
 
 app.use("/them-sp", (req, res, next) => {
-  console.log("In the middleware!");
-  res.send("<h1>add product</h1>");
+  res.send(
+    "<form action='/san-pham'><input type='text' name='title'><button type='submit'>Them SP</button></form>"
+  );
+});
 
+app.use("/san-pham", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In another middleware!");
   res.send("<h1>xin chao</h1>");
 }); // index phải là middleware cuối cùng
 
