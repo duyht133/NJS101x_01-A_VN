c2269aadc4f09b00112471fe11726e6ad4e2d947
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-07 00:42:14 +0700

    lab2-1

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
-const routes = require('./routes');
-
+/* const routes = require('./routes');
 console.log(routes.sometext)
+const server = http.createServer();
+server.listen(3000); */
 
-const server = http.createServer(routes.handler);
-server.listen(3000);
+
+
+// GET EXPRESS
+ const express = require('express');
+// CALL EXPESS FORM FUNCTION
+const app = http.createServer(express());
+app.listen(3000);
