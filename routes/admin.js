bd7e99bb99065744592f5fe840a803b3a6bb8724
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-08 01:15:54 +0700

    lab2-11-patch-require-fix

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,17 @@
+
+//import require patch
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/them-sp', (req, res, next) => {
-  res.send(
-    '<form action="/san-pham" method="POST"><input type="text" name="title"><button type="submit">Them SP</button></form>'
-  );
+  res.sendFile(path.join(__dirname, '../', 'views', 'them-sp.html'));
 });
 
+// /admin/add-product => POST
 router.post('/san-pham', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
