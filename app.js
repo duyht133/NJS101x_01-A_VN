7b03818e19918069fdc221f8fd60cad78981fe96
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-08 00:41:46 +0700

    lab2-9

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+//phân lường đường dẫn, thêm patch /admin làm đường dẫn đầu vào
+app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
         res.status(404).send('<h1>page not found</h1>');
-
 })
 
 app.listen(3000);
