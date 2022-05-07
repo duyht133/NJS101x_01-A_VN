fa0fe861999b5c6ef83f59823d19c79fe2b370d5
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-07 13:18:42 +0700

    lab2-8

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const express = require('express');
 const bodyParser = require('body-parser');
-
-const app = express();
-
 const shopRoutes = require('./routes/shop');
 const adminRoutes = require('./routes/admin');
+const express = require('express');
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+        res.status(404).send('<h1>page not found</h1>');
+
+})
 
 app.listen(3000);
