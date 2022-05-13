8fee05b545e3fd2045273e77e2dbc78a8cab7895
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-14 00:01:11 +0700

    lab4-4_SaveDataUsingModel

--- models/product.js
+++ models/product.js
@@ -1,15 +1,31 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const p = path.join(path.dirname(process.mainModule.filename), "data", "products.json");
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    save() {
-        products.push(this);
-    }
+  save() {
+    fs.readFile(p, (err, data) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(data);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll() {
-        return products;
-    }
-}
+  static fetchAll() {
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        return [];
+      }
+      return Json.parse(data);
+    });
+  }
+};
