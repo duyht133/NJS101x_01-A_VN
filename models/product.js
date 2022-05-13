3f01f16befaf7813132a7a925799ed9284081d69
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-14 01:01:39 +0700

    lab4-6_conceptFile

--- models/product.js
+++ models/product.js
@@ -2,30 +2,31 @@ const fs = require("fs");
 const path = require("path");
 const p = path.join(path.dirname(process.mainModule.filename), "data", "products.json");
 
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, data) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(data));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    fs.readFile(p, (err, data) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(data);
-      }
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
+    getProductsFromFile(products => {
+        products.push(this);
+        fs.writeFile(p, JSON.stringify(products), err => {
+          console.log(err);
+        });
       });
-    });
   }
 
   static fetchAll(cb) {
-    fs.readFile(p, (err, data) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(data));
-    });
+    getProductsFromFile(cb);
   }
 };
