2de88331212501ecb75ad977e0166f4d73574a5a
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-20 00:49:01 +0700

    lab5-5

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,15 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
+  Product.fetchAll()
+  .then(([rows, fieldData]) => {
     res.render("shop/product-list", {
-      prods: products,
+      prods: rows,
       pageTitle: "All Products",
       path: "/products",
     });
-  });
+  })
+  .catch(err => console.log(err));
 };
 
 exports.getProduct = (req, res, next) => {
@@ -24,7 +26,7 @@ exports.getProduct = (req, res, next) => {
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll()
-    .then(([rows, fielData]) => {
+    .then(([rows, fieldData]) => {
       res.render("shop/index", {
         prods: rows,
         pageTitle: "Shop",
