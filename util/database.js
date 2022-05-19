d889466de9c738e46f2ff4d74afd13f8063881d2
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-20 00:18:36 +0700

    lab5-1_5-2_ConnectionAndCreateData

---
+++ util/database.js
@@ -0,0 +1,12 @@
+// Using connection data Mysql
+
+const mysql = require('mysql');
+
+// create pool (tạo pool để chứa các thông tin kết nối với Mysql)
+const pool = mysql.createPool({
+    host: 'localhost', // host website
+    user: 'root', // use name Mysql
+    database: 'node-complete', // name database
+    password: 'concat1233' // password connection user
+})
+module.exports = pool.promise();
