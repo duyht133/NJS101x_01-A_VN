4e64c6e50c4f0ea71d13dd39595d1541b74201aa
duyht133 <duyhtfx13324@funix.edu.vn>
2022-05-22 02:49:07 +0700

    lab5-28_resettingCart_FetchingAndOutputOrder

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
-
 const Order = require('./models/order');
 const OrderItem = require('./models/order-item');
 
@@ -46,12 +45,11 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 Order.belongsTo(User);
 User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem})
-
+Order.belongsToMany(Product, { through: OrderItem });
 
 sequelize
-  .sync({ force: true })
-  // .sync()
+  // .sync({ force: true })
+  .sync()
   .then(result => {
     return User.findByPk(1);
     // console.log(result);
@@ -63,9 +61,10 @@ sequelize
     return user;
   })
   .then(user => {
+    // console.log(user);
     return user.createCart();
   })
-  .then(() => {
+  .then(cart => {
     app.listen(3000);
   })
   .catch(err => {
