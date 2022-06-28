873bf7d31da89d98549f28f21f4127c9a9082ad1
duyht133 <duyhtfx13324@funix.edu.vn>
2022-06-28 22:50:31 +0700

    lab8-7_UsingMONGODB_Store_Session

--- app.js
+++ app.js
@@ -2,27 +2,38 @@ const path = require("path");
 
 const express = require("express");
 const mongoose = require("mongoose");
-const session =require("express-session")
+const session = require("express-session");
+const MongoDBstore = require("connect-mongodb-session")(session);
 
 const User = require("./models/user");
 
+const MONGODB_URI =
+  "mongodb+srv://admin:admin@cluster0.gphjhaw.mongodb.net/?retryWrites=true&w=majority";
+
 const app = express();
+const store = new MongoDBstore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const authRoutes = require('./routes/auth');
+const authRoutes = require("./routes/auth");
 const errorController = require("./controllers/error");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({
-  secret: 'my secret',
-  resave: false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("62bb0e9b7b4e52dcfc7df741")
@@ -39,9 +50,10 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect("mongodb+srv://admin:admin@cluster0.gphjhaw.mongodb.net/?retryWrites=true&w=majority")
+  .connect(MONGODB_URI)
   .then(() => {
-    User.findOne().then((user) => { // lọc điều kiện để tránh tạo user mới mỗi lần reset server
+    User.findOne().then((user) => {
+      // lọc điều kiện để tránh tạo user mới mỗi lần reset server
       if (!user) {
         const user = new User({
           name: "Duyht",
