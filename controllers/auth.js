2c33352579092b207a8ea9a0e8bb2ee0329c9766
duyht133 <duyhtfx13324@funix.edu.vn>
2022-06-28 23:01:19 +0700

    lab8-7_UsingMONGODB_Store_Session

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,26 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  /* const isLoggedIn = req.get("Cookie").trim().split("=")[1] === "true"; */
-  console.log(req.session.isLoggedIn);
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
-    isAuthenticated: false,
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect("/");
+  User.findById('62bb0e9b7b4e52dcfc7df741')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
