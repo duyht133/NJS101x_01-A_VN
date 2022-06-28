b063cda3360daea424257ebc10cd6b69474a57f5
duyht133 <duyhtfx13324@funix.edu.vn>
2022-06-28 23:25:07 +0700

    lab8-10_AddToCartWorking

--- app.js
+++ app.js
@@ -36,6 +36,18 @@ app.use(
 );
 
 app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use((req, res, next) => {
   User.findById("62bb0e9b7b4e52dcfc7df741")
     .then((user) => {
       req.user = user;
